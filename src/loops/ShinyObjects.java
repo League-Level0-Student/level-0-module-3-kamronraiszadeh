7680734ee6ed6f1f38a3116c87649c7efd436673
peter <league@WTS10.attlocal.net>
2018-04-04 16:26:43 -0700

    shiny objects, walk of fame

--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -5,17 +5,22 @@ package loops;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 
 		// 2. Ask the user how many shiny objects they want
-
+    String answer= JOptionPane.showInputDialog("How many shiny oobjects would you like?");
 		// 3. Play the sound that many times
-
+    int num=      Integer.parseInt(answer); 
+    
+    for (int i = 1; i <= num; i++) {
+    	playMisterZee();
+		}
 		// 1. Call the method below
-
+		playMisterZee();
 	}
 
 	public static void playMisterZee() {
--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -11,11 +11,19 @@ public class WalkOfFame {
 		Robot rob = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+       rob.hide();
+		rob.setSpeed(10);
+        rob.penDown();
+        rob.setPenColor(0,255,0);
+        rob.setPenWidth(30);
+		rob.setX(150);
 		// 2. Make the robot draw a star shape. Hint: 144.
-
+      for (int i = 0; i <6; i++) {
+		rob.turn(144);
+		rob.move(100);
+	}
 		// 3. Set the size of the star to 30.
-
+        
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of stars like this:
